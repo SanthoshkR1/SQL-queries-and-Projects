0f947f32510d4609a6a9d2f4c90e482bd7a2c37a
SanthoshkR1 <santhoshkr366@gmail.com>
2025-08-24 08:50:54 +0530

    Updated Where Clause SQL file

--- Where_Clause.sql
+++ Where_Clause.sql
@@ -28,7 +28,46 @@ FROM employee_demographics
 WHERE birth_date >'1985-01-01'
 ;
 
+SELECT *
+FROM employee_demographics
+WHERE birth_date >'1985-01-01'
+AND gender = 'male'
+;
+SELECT *
+FROM employee_demographics
+WHERE birth_date >'1985-01-01'
+OR gender = 'male'
+;
+
+-- AND OR NOT -- logical Operators
+SELECT * 
+FROM employee_demographics
+WHERE (first_name='Leslie' AND age = 44) OR age>55
+;
+
+SELECT * 
+FROM employee_demographics
+WHERE birth_date >'1985-01-01'
+AND gender ='male'
+;
+
+SELECT *
+FROM employee_demographics
+WHERE birth_date > '1985-01-01'
+OR gender ='male'
+;
+
+SELECT *
+FROM employee_demographics
+WHERE birth_date > '1985-01-01'
+OR NOT gender ='male'
+;
 -- Here '1990-01-01' is the default data formate in MySQL.
+
+SELECT *
+FROM employee_demographics
+WHERE (first_name ='leslie' AND age = 44) OR age > 55
+;
 
 #LIKE STATEMENT 
 -- two special characters a % and a 
@@ -42,8 +81,33 @@ WHERE first_name LIKE 'a%';
 -- _ means a specific value
 SELECT *
 FROM employee_demographics
+WHERE first_name LIKE 'a__'
+;
+
+SELECT *
+FROM employee_demographics
+WHERE first_name LIKE 'a___'
+;
+
+SELECT *
+FROM employee_demographics
 WHERE first_name LIKE 'a__%';
 
 SELECT * 
 FROM employee_demographics
 WHERE first_name LIKE 'a___%';
+
+SELECT * 
+FROM employee_demographics
+WHERE first_name LIKE 'Jer%'
+;
+
+SELECT * 
+FROM employee_demographics
+WHERE first_name LIKE '%er%'
+;
+
+SELECT *
+FROM employee_demographics
+WHERE birth_date LIKE '1989%'
+;
